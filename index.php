e01d93ee087eb7d109a10c874b0a309cbb6a53e8
shynzx <herreraloezae@gmail.com>
2024-07-31 21:31:44 -0500

    commit

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="../../css/style_index.css" />
+		<link rel="stylesheet" href="../../css/normaliz.css" />
+		<link
+			rel="shortcut icon"
+			href="/favicon_io/favicon.ico"
+			type="image/x-icon"
+		/>
+		<title>JET</title>
+	</head>
+	<body>
+		<header>
+			<div class="marca">
+				<a href="index.php">
+					<img src="../../img/marca.png" alt="" />
+				</a>
+			</div>
+			<div class="login">
+				<div class="inicio_sesion">
+					<a href="login.html"> Inicio de sesion </a>
+				</div>
+			</div>
+		</header>
+
+		<main>
+			<section class="welcom">
+				<div class="contet-welcom">
+					<p>Bienvenido a Just Enough Time</p>
+					<h2>
+						Organizador <br/>
+						de <br/>
+						Tareas y Notas
+					</h2>
+				</div>
+			</section>
+
+			<section class="funciones" id="nosotros">
+				<div class="uso" >
+					<h2>Nosotros</h2>
+				</div>
+				<div class="nosotros">
+					<p>
+						La empresa se dedica al desarrollo de software, especializándose en
+						aplicaciones web y sistemas de gestión que facilitan la organización
+						y seguimiento de actividades académicas, laborales y personales. Se
+						enfoca en crear soluciones tecnológicas funcionales, ergonómicas y
+						efectivas, destinadas a mejorar la productividad y gestión de sus
+						usuarios.
+					</p>
+					
+				</div>
+			</section>
+		</main>
+
+		<section class="conjunto_funciones" id="objetivo">
+			<div class="compromiso" >
+				<p>
+					En JET, estamos profundamente comprometidos con agilizar el día a día
+					de nuestros usuarios, ayudándolos e impulsándolos a cumplir con sus
+					tareas diarias de manera más eficiente y efectiva. Nuestro objetivo es
+					proporcionar herramientas y soluciones innovadoras que simplifiquen la
+					gestión del tiempo, permitiendo a nuestros usuarios enfocarse en lo
+					que realmente importa.
+				</p>
+			</div>
+
+			<section id="funciones">
+				<div class="uso funcion" >
+					<h2>Funciones</h2>
+				</div>
+
+				<div class="reticula">
+					<div class="tarea">
+						<h2>Gestión de Tareas</h2>
+						<p>
+							Organiza y prioriza tus tareas diarias con nuestra herramienta de
+							gestión de tareas, diseñada para ayudarte a mantener el control y
+							no perder de vista tus objetivos.
+						</p>
+					</div>
+					<div class="notas">
+						<h2>Gestión de notas</h2>
+						<p>
+							Captura y organiza tus ideas y pensamientos en cualquier momento
+							con nuestra funcionalidad de notas. Puedes crear, editar y acceder
+							a tus notas de manera rápida y sencilla, asegurando que nunca
+							pierdas una idea importante.
+						</p>
+					</div>
+					<div class="agenda">
+						<h2>Calendario</h2>
+						<p>
+							Nunca olvides una cita o una fecha importante con nuestro
+							calendario integrado y sistema de recordatorios. Mantente siempre
+							al tanto de tus compromisos.
+						</p>
+					</div>
+				</div>
+
+				<div class="compromiso uso" id="compromiso">
+					<h2>Nuestro Compromiso</h2>
+					<p>
+						nuestro compromiso es ser más que una simple herramienta de gestión
+						del tiempo. Queremos ser un socio en tu camino hacia la eficiencia y
+						el éxito, proporcionándote todo lo necesario para que cada día sea
+						más productivo y menos estresante.
+					</p>
+				</div>
+			</section>
+		</section>
+
+		<footer>
+			<div class="footer_container" id="footer">
+				<div>
+					<a href="aviso_privacidad.html"> Aviso de privacidad </a>
+				</div>
+				<div>
+					<a href="terminos_condiciones.html"> Terminos y condiciones </a>
+				</div>
+				<div>
+					<a href="mapa.html"> Mapa del sitio </a>
+				</div>
+			</div>
+			<div class="derechos">
+				Todos los derechos reservados por Santana Saste Roger, Herrera Loeza
+				Enrique y Uc Pool Francisco &copy;
+			</div>
+		</footer>
+	</body>
+</html>
