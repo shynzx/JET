09a25935e64457d7ae23269d9b0da1beb7ef16f3
shynzx <herreraloezae@gmail.com>
2024-08-02 13:53:56 -0500

    git commit

--- Jet.V2/config/conection.php
+++ Jet.V2/config/conection.php
@@ -9,15 +9,20 @@ class DataBase {
     public function __construct() {
         self::$localhost = 'jet.mysql.database.azure.com';
         self::$database = 'jet';
-        self::$username = 'enrique'; 
+        self::$username = 'enrique@jet'; // Nota el '@jet' al final del username.
         self::$password = '125128HL:v125'; 
         self::connect();
     }
 
     private static function connect() {
         try {
+            $options = [
+                PDO::MYSQL_ATTR_SSL_CA => 'BaltimoreCyberTrustRoot.crt.pem', // Ruta al certificado SSL
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ];
             self::$databaseConnection = new PDO(
-                "mysql:host=" . self::$localhost . ";dbname=" . self::$database, self::$username, self::$password
+                "mysql:host=" . self::$localhost . ";dbname=" . self::$database, self::$username, self::$password, $options
             );
         } catch (PDOException $error) {
             echo "Failed connection to database: " . $error->getMessage();
@@ -33,4 +38,3 @@ class DataBase {
     }
 }
 ?>
-
